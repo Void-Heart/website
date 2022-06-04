1cc7b9449c0ea61d57f59ca43e48094b1e0f2a9b
LukeG007 <luke@gilsinger.net>
2022-06-04 17:02:44 -0400

    Finish python commit history, start on svelte candidate

--- main.py
+++ main.py
@@ -1,18 +1,42 @@
 from flask import Flask, render_template, redirect, request, make_response
+import requests
+from requests.auth import HTTPBasicAuth
+import os
 
 app = Flask(__name__)
+if os.path.exists('key'):
+    keyf = open('key', 'r')
+    key = keyf.read().split(':')
+    keyf.close()
+    devmode = False
+else:
+    devmode = True
+    key = None
+if devmode:
+    print('DevMode: ON')
+else:
+    print('DevMode: OFF')
 
-@app.route('/')
-def home():
+def get_commit_id(sha):
+    return sha[:5]
+
+@app.route('/commits')
+def commit_history():
     cookies = dict(request.cookies)
     cookie_not_found = False
     if not 'theme' in cookies:
         cookies['theme'] = 'dark'
         cookie_not_found = True
-    resp = make_response(render_template('home.html', cookies=cookies))
+    if not devmode:
+        print('https://api.github.com/repos/{}/commits?page=1'.format(key[0]))
+        github_data = requests.get('https://api.github.com/repos/{}/commits?page=1'.format(key[0]), auth=HTTPBasicAuth(key[1], key[2])).json()
+    else:
+        github_data = []
+    print(github_data)
+    resp = make_response(render_template('home.html', cookies=cookies, github_data=github_data, devmode=devmode, get_commit_id=get_commit_id))
     if cookie_not_found:
         resp.set_cookie('theme', cookies['theme'])
-    return render_template('home.html', cookies=cookies)
+    return resp
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5647)
