fb98a8ebd6cbe6f2dd4b6cb4508640ad143ec53f
LukeG007 <luke@gilsinger.net>
2022-06-07 00:30:02 -0400

    Remove more paging

--- sveltekit-candidate/src/routes/commits/index.js
+++ sveltekit-candidate/src/routes/commits/index.js
@@ -1,5 +1,5 @@
 /** @type {import('./__types').RequestHandler} */
 export const get = async ({ locals }) => {
     // @ts-ignore
-    return {body: {github_data: locals.github_data, page: locals.page, page_back: locals.page_back, page_next: locals.page_next}}
+    return {body: {github_data: locals.github_data}}
 }
