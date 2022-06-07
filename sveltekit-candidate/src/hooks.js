994a0c9674fb9606cc7cf28375e156307c426f2e
LukeG007 <luke@gilsinger.net>
2022-06-07 00:00:32 -0400

    Remove pages in commits to make it compatible with the API again

--- sveltekit-candidate/src/hooks.js
+++ sveltekit-candidate/src/hooks.js
@@ -1,25 +1,52 @@
-import fs from "node:fs"
 /** @type {import('@sveltejs/kit').Handle} */
 export const handle = async ({ event, resolve }) => {
     if (event['url']['pathname'] == '/commits') {
-        let page = 1;
+        let page = "1";
+        let next_page = "2";
+        let back_page = "1";
         if (event.url.searchParams.get('page') != null) {
             // @ts-ignore
             page = event.url.searchParams.get('page');
+            next_page = (Number(page)+1).toString();
+            back_page = (Number(page)-1).toString();
+            if (back_page == "0") {
+                back_page = "1";
+            }
         }
-        let key = fs.readFileSync('key', 'utf-8').split(':')
-        let url = 'https://api.github.com/repos/' + key[0] + '/commits?page=' + page
-        let headers = new Headers();
-        headers.set('Authorization', 'Basic ' + Buffer.from(key[1] + ":" + key[2]).toString('base64'));
-        const resp = fetch(url, {
-            method: 'GET',
-            headers: headers
-        });
+        let github_data = await get_github_data(event, page);
         // @ts-ignore
-        event.locals.github_data = await (await resp).json();
-        // @ts-ignore
-        event.locals.page = page;
+        event.locals.github_data = github_data;
     }
     const response = await resolve(event);
     return response;
+}
+
+// @ts-ignore
+async function get_github_data(event, page) {
+    let tryagain = false;
+    let url = 'http://localhost:5636/get_commit_history';
+    var data = [];
+    try {
+        // @ts-ignore
+        const resp = await fetch(url, {
+            method: 'GET'
+        })
+        // @ts-ignore
+        data = await resp.json();
+    }
+    catch(err) {
+        // @ts-ignore
+        if (err['errno'] == 'ERR_STREAM_PREMATURE_CLOSE') {
+            tryagain = true;
+        } else {
+            throw err
+        }
+    }
+    finally {
+        if (tryagain == true) {
+            // @ts-ignore
+            data = get_github_data(event, page);
+        }
+    }
+    return data;
 }
--- sveltekit-candidate/src/routes/commits/index.js
+++ sveltekit-candidate/src/routes/commits/index.js
@@ -1,7 +1,5 @@
-import { page } from '$app/stores';
-
 /** @type {import('./__types').RequestHandler} */
 export const get = async ({ locals }) => {
     // @ts-ignore
-    return {body: {github_data: locals.github_data, page: locals.page}}
+    return {body: {github_data: locals.github_data, page: locals.page, page_back: locals.page_back, page_next: locals.page_next}}
 }
