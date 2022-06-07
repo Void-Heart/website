1938f915f80746328c1c35fe155e18578b7b2775
LukeG007 <luke@gilsinger.net>
2022-06-07 13:01:07 -0400

    Make usernames turn to credit names Add icon to navbar

--- sveltekit-candidate/src/routes/commits/index.svelte
+++ sveltekit-candidate/src/routes/commits/index.svelte
@@ -1,12 +1,26 @@
 <script> 
     export let github_data;
+    function get_name(name) {
+        if (name == "LukeG007") {
+            return "natma_relniame";
+        } else if (name == "LoganToast") {
+            return "Cosmos";
+        } else if (name == "BruhMan669") {
+            return "RetardedKid";
+        } else if (name == "Eros-Hai") {
+            return "Eros_Hai"
+        }
+        return name;
+    }
 </script>
+
+<title>Void Heart - Commit History</title>
 
 <div class="card-group">
     {#each github_data['commit_data'] as data}
         <div class="card">
             <h3 class="card-text">{data['message']}</h3>
-            <h3 class="card-text">- {data['author']['name']}</h3>
+            <h3 class="card-text">- {get_name(data['author']['name'])}</h3>
         </div>
     {/each}
 </div>
---
+++ sveltekit-candidate/src/routes/index.svelte
@@ -0,0 +1,1 @@
+<title>Void Heart</title>
